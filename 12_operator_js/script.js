3ec71b18ffa699d03a2cb9e85e44fbc1a4292305
Rafaely Zanão <rafaelyobzutzanao@gmail.com>
2025-02-08 11:08:12 -0300

    operadores em JS

---
+++ 12_operator_js/script.js
@@ -0,0 +1,22 @@
+var idade = 16;
+var possuiCarro = 0;
+
+if(idade >= 18){
+     console.log("O usuario pode fazer a carteira");
+}
+
+if( idade <= 17){
+    console.log("O usuario nao pode fazer a carteira");
+}
+
+if(possuiCarro){
+     console.log("Usuario já pode andar de carro");
+}
+
+var nome = "Gabi";
+
+if(nome == "Rafaely"){
+    console.log("Seu nome é Rafaely")
+}else{
+    console.log("Nome não correspondente")
+}
